4d7534bc15e51acdcb6bb2cc85aa4366f9e34c3b
Latisha Johnson <latishaj966@gmail.com>
2021-03-23 12:23:53 -0700

    fixed typo in command

--- shoeapp/management/commands/bootstrap_data.py
+++ shoeapp/management/commands/bootstrap_data.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from shoeapp.models import ShoeType, ShoeColor
 
 class Command(BaseCommand):
-    help = 'Adds type of Shoe Color for Database'
+    help = 'Adds type of Shoe and Color for Database'
     def add_arguments(self, parser):
         parser.add_argument('style', type=str, nargs=5, help='Options: sneaker, boot, sandal, dress, other')
         parser.add_argument('color_name', choices=['red','orange','yellow','green','blue','indigo','violet','white','black'], type=str, nargs=9, help='Options: red, orange, yellow, green, blue, indigo, violet, white, black')
