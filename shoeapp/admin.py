483b720c525232743e04b5b5121b24a1c2fe9718
Latisha Johnson <latishaj966@gmail.com>
2021-03-23 09:07:41 -0700

    added a home page and added admin info

--- shoeapp/admin.py
+++ shoeapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from shoeapp.models import Shoe, ShoeColor, ShoeType, Manufacturer
 # Register your models here.
+admin.site.register(Shoe)
+admin.site.register(ShoeType)
+admin.site.register(ShoeColor)
+admin.site.register(Manufacturer)
