73236b63e0f9604cd12722c5e335376807455541
Latisha Johnson <latishaj966@gmail.com>
2021-03-25 08:58:38 -0700

    added allow all orgins for cors

--- shoestoreserver/settings.py
+++ shoestoreserver/settings.py
@@ -56,10 +56,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",
-    "http://127.0.0.1:8000"
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'shoestoreserver.urls'
 
